88187572e7c48f26afa69a974b9d5cabd124eff7
임t <ssep4u@gmail.com>
2024-05-13 12:16:32 +0900

    views CharacterListView

--- 콩순이/views.py
+++ 콩순이/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from 콩순이.models import Character
+
+
+class CharacterListView(ListView):
+    model = Character
+    # character_list = Character.objects.all()  #DB에 모델 데이터 다 가져오자
+    # return render(request, '콩순이/character_list.html', context={'character_list': character_list})     #모델_list.html에 모델_list라는 키로 DB에서 가져온 데이터 넣어서 render 하자
